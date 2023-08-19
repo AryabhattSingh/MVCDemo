3331ac375368bc6c5737f8ff77e11fba0bf10bca
Aryabhatt <bhattsahab314@gmail.com>
2023-08-19 14:49:21 +0530

    Created the Model. Contains a function that returns the required average.

---
+++ src/main/java/mvcDemo/AverageCalculator.java
@@ -0,0 +1,9 @@
+package mvcDemo;
+
+public class AverageCalculator {
+
+	public double calcuateAverage(double firstNumber, double secondNumber) {
+		return (firstNumber + secondNumber) / 2;
+	}
+
+}
